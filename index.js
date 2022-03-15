46751837e3a7386a9039a936e820c4e51155f96a
Marcel Wlotzka <marcel@wlotzka.org>
2022-03-15 13:41:47 +0100

    Added comments and cleanup

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const { window } = new JSDOM();
 const $ = jq(window);
 const targetDir = './output/package/com.visualstudio.code.sdIconPack';
 
+/**
+ * Copies a directory with all its children from a to b
+ * @param {string} src 
+ * @param {string} dest 
+ */
 const copyDir = async (src, dest) => {
   const entries = await fs.readdir(src, { withFileTypes: true });
   await fs.mkdir(dest, { recursive: true } );
@@ -27,20 +32,26 @@ const copyDir = async (src, dest) => {
   }
 };
 
-const buildCodiconSVG = async () => {
-  // Checkout current vscode-codicons repository
+/**
+ * Checkout the recent version of microsofts codicons repository
+ * @returns version number of microsofts repository
+ */
+const checkoutVSCodeCodIconsRepo = async () => {
   let gitOtions = {
     baseDir: path.join(process.cwd(), './vscode-codicons'),
   };
 
   if (await dirExists(gitOtions.baseDir)) {
+    // clean and pull
     simpleGit(gitOtions)
       .clean(CleanOptions.FORCE)
       .pull();
   } else {
+    // Checkout current vscode-codicons repository
     await simpleGit().clone('https://github.com/microsoft/vscode-codicons.git', gitOtions.baseDir);
   }
 
+  // Get version number from microsoft
   let version = JSON.parse(await fs.readFile(path.join(gitOtions.baseDir, 'package.json'), 'utf-8')).version;
 
   console.log(`https://github.com/microsoft/vscode-codicons.git version: ${version}`);
@@ -48,16 +59,24 @@ const buildCodiconSVG = async () => {
   return version;
 };
 
+/**
+ * Generates plugin files
+ * @param {string} version 
+ */
 const generateIcons = async (version) => {
+  // Create temp directories
   await fs.mkdir('./output/tmp', { recursive: true });
   await fs.mkdir(`${targetDir}/icons`, { recursive: true });
 
+  // Get SVG source files
   let files = (await fs.readdir('./vscode-codicons/src/icons')).map(e => e.replace('.svg', ''));
 
+  // Read mappings in order to generate tags on icons
   let mappings = JSON.parse(await fs.readFile('./vscode-codicons/src/template/mapping.json'));
 
   let icons = {};
 
+  // initialize icons object with name, path and empty tags
   files.forEach(name => {
     let id = mappings[name];
 
@@ -68,6 +87,7 @@ const generateIcons = async (version) => {
     };
   });
 
+  // For each mapping check if there is already an entry. If not add the name to the tag list
   icons = Object.values(Object.keys(mappings).reduce((last, name) => {
     let id = mappings[name];
 
@@ -78,29 +98,37 @@ const generateIcons = async (version) => {
     return last;
   }, icons));
 
-  let promises = icons.map(async file => {
+  // Update each icon to match Stream Deck layout and size
+  const promises = icons.map(async file => {
     let name = file.name;
 
+    // read the icons source
     let icon = await fs.readFile(`./vscode-codicons/src/icons/${name}.svg`, 'utf-8');
-    
-    icon = icon.replace('currentColor', 'white');
 
+    // Append the icon to a jQuery div for easier manipulation
     let html = $('<div />').append(icon);
 
+    // Find out the original icon size
     let originalSize = parseInt(html.find('svg').attr('width'), 10);
 
+    // Set target color and size
     html.find('svg')
       .attr('width', '144')
       .attr('height', '144')
+      .attr('fill', 'white')
       .removeAttr('viewBox');
 
+    // Transform each path object of the svg to fit into new size
     html.find('path').toArray().forEach(el => {
       let jqel = $(el);
       let path = jqel.attr('d');
+
+      // icon padding for all directions
       let padding = 32;
+
       let newPath = new SVGPathCommander(path).transform({
-        translate: [padding, padding],
-        scale: (144 - (padding * 2)) / originalSize,
+        translate: [padding, padding], // move to the padding size
+        scale: (144 - (padding * 2)) / originalSize, // calculate the scale depending on padding and original size
         origin: [0, 0]
       }).toString();
       
@@ -109,18 +137,23 @@ const generateIcons = async (version) => {
     
     icon = html.html();
 
+    // Write the new svg file to the target directory
     fs.writeFile(`${targetDir}/icons/${name}.svg`, icon);
   });
 
+  // Wait for all icons to complete
   await Promise.all(promises);
 
+  // Save all icon information
   await fs.writeFile(`${targetDir}/icons.json`, JSON.stringify(icons, null, 2));
 
+  // Copy assets
   await fs.cp('./assets/cover.png', `${targetDir}/cover.png`);
   await fs.cp('./assets/icon.png', `${targetDir}/icon.png`);
   await fs.cp('./assets/license.txt', `${targetDir}/license.txt`);
   await copyDir('./assets/previews', `${targetDir}/previews`);
 
+  // Set manifest version and save it to the target directory
   let manifest = JSON.parse(await fs.readFile('./assets/manifest.json'));
 
   manifest.Version = version;
@@ -128,13 +161,17 @@ const generateIcons = async (version) => {
   await fs.writeFile(`${targetDir}/manifest.json`, JSON.stringify(manifest, null, 2));
 };
 
-const run = async () => {
-  let version = await buildCodiconSVG();
+/**
+ * Helper function needed for running the async methods
+ */
+(async () => {
+  // Checkout microsofts original repository
+  let version = await checkoutVSCodeCodIconsRepo();
+
+  // Generate icon plugin directory
   await generateIcons(version);
 
+  // create streamDeckIconPack zip file
   await zip('./output/package', './output/com.visualstudio.code.sdIconPack.streamDeckIconPack');
-
   console.log('com.visualstudio.code.sdIconPack.streamDeckIconPack written.');
-};
-
-run();
+})();
